496da1763ffedd746a3f16a9b046e1112206789e
ademidun <tomiademidun@gmail.com>
2020-08-23 18:39:53 -0400

    pass a message from popup to content script (saveScholarship.js) to save the scholarship on the current page and pass date picker value

--- options.js
+++ options.js
@@ -1,7 +1,5 @@
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-constructOptions(kButtonColors);
 createSavedScholarshipsList();
+
 function createSavedScholarshipsList() {
 
     chrome.storage.sync.get("savedScholarships", function(items) {
@@ -31,17 +29,4 @@ function createSavedScholarshipsList() {
 
 
 
-}
-
-
-function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-        let button = document.createElement('button');
-        button.style.backgroundColor = item;
-        button.addEventListener('click', function() {
-            chrome.storage.sync.set({color: item}, function() {
-            })
-        });
-        page.appendChild(button);
-    }
 }
--- saveScholarship.js
+++ saveScholarship.js
@@ -1,34 +1,54 @@
-console.log('saveScholarship');
-console.log('document.URL', document.URL);
-console.log('document.title', document.title);
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
 
-chrome.storage.sync.get("savedScholarships", function(items) {
-    let savedScholarships = [];
-    const url = document.URL;
-    // using name instead of title here,
-    // the Atila API uses refers to scholarships with 'name'
-    const name = document.title;
-    const deadline = new Date().toISOString();
+        console.log({request});
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
 
-    const scholarshipData = {
-        url,
-        name,
-        deadline,
-    };
+        if (request.messageType === "saveScholarship") {
 
-    console.log({items});
-
-    if (items.savedScholarships) {
-        savedScholarships = items.savedScholarships
-    }
-    savedScholarships.push(scholarshipData);
-
-    chrome.storage.sync.set({ "savedScholarships" : savedScholarships }, function() {
-
-        if (chrome.runtime.lastError) {
-            console.log(`Runtime error. ${chrome.runtime.lastError}`);
+            const scholarship = request.scholarship;
+            saveScholarship(scholarship, sendResponse);
         }
+
+        return true;
 
     });
 
-});
+
+function saveScholarship(scholarship, sendResponse) {
+    chrome.storage.sync.get("savedScholarships", function(items) {
+        let savedScholarships = [];
+        const url = document.URL;
+        // using name instead of title here,
+        // the Atila API uses refers to scholarships with 'name'
+        const name = document.title;
+        const deadline = scholarship.deadline;
+        const dateAdded = new Date();
+
+        const scholarshipData = {
+            url,
+            name,
+            deadline,
+            dateAdded,
+        };
+
+        console.log({items});
+
+        if (items.savedScholarships) {
+            savedScholarships = items.savedScholarships
+        }
+        savedScholarships.push(scholarshipData);
+
+        chrome.storage.sync.set({ "savedScholarships" : savedScholarships }, function() {
+
+            if (chrome.runtime.lastError) {
+                console.log(`Runtime error. ${chrome.runtime.lastError}`);
+            }
+            sendResponse({savedScholarships});
+
+        });
+
+    });
+}
