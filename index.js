3f44dcd2e3b3673cb01a213021189de1a6648f56
ademidun <tomiademidun@gmail.com>
2021-09-05 23:19:05 -0400

    add bootstrap stylings to saved scholarships

--- index.js
+++ index.js
@@ -11,20 +11,49 @@ function createSavedScholarshipsList() {
         }
 
         const container = document.getElementById('savedScholarshipsListContainer');
-        const ul = document.createElement('ul');
-        container.appendChild(ul); // append the created ul to the root
 
+        const table = document.createElement('table');
+        table.classList.add("table")
+
+        const thead = document.createElement('thead');   
+        let tr = document.createElement('tr');   
+
+        const th1 = document.createElement('th');
+        const th2 = document.createElement('th');
+
+        const text1 = document.createTextNode('Name');
+        const text2 = document.createTextNode('Deadline');
+        th1.appendChild(text1);
+        th2.appendChild(text2);
+        tr.appendChild(th1);
+        tr.appendChild(th2);
+        thead.appendChild(tr);
+        table.appendChild(thead);
+
+
+        const tbody = document.createElement('tbody');
         savedScholarships.forEach(function(savedScholarship) {
+            let paragraph = document.createElement('p'); // create a new list savedScholarship
 
+            paragraph.innerHTML = `<a href="${savedScholarship.url}" target="_blank" rel="noopener noreferrer">
+            ${savedScholarship.name}</a>`
 
-            const li = document.createElement('li'); // create a new list savedScholarship
+            tr = document.createElement('tr');   
+            td1 = document.createElement('td');
+            td2 = document.createElement('td');
 
-            li.innerHTML = `<a href="${savedScholarship.url}" target="_blank" rel="noopener noreferrer">
-            ${savedScholarship.name}</a>
-            <br/>
-            Due: <small>(${savedScholarship.deadline})</small>`
-            ul.appendChild(li);
+            const deadlineText = document.createTextNode(`${savedScholarship.deadline}`);
+
+            td1.appendChild(paragraph);
+            td2.appendChild(deadlineText);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+
+            tbody.appendChild(tr);
+
         });
+        table.appendChild(tbody);
+        container.appendChild(table);
     });
 
 
