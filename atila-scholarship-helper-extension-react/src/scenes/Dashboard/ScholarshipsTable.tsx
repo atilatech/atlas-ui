7a11462837ed629ddb7f3579197c583ee42f4462
ademidun <tomiademidun@gmail.com>
2021-09-07 20:28:42 -0400

    moved scolarship table row into seperate component

--- atila-scholarship-helper-extension-react/src/scenes/Dashboard/ScholarshipsTable.tsx
+++ atila-scholarship-helper-extension-react/src/scenes/Dashboard/ScholarshipsTable.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Scholarship } from '../../models/Scholarship';
-import { ScholarshipUtils } from '../../services/ScholarshipUtils';
 import { Utils } from '../../services/Utils';
 import './ScholarshipsTable.css';
+import { ScholarshipTableRow } from './ScholarshipTableRow';
 
 const scholarshipsTableId = "ScholarshipsTable";
 function ScholarshipsTable() {
@@ -72,55 +72,6 @@ function ScholarshipsTable() {
 
       </div>
     );
-}
-
-interface ScholarshipTableRowProps {
-    scholarship: Scholarship
-};
-
-export function ScholarshipTableRow(props: ScholarshipTableRowProps) {
-
-    const { scholarship } = props;
-
-    const scholarshipRowId = `scholarship-row-${scholarship.id}`;
-    const copyToClipBoardRowId = `copyToClipBoard-${scholarshipRowId}`;
-
-    const copyToClipBoard = () => {
-      
-      const toastBody = `Tip: Open <a href="https://sheets.new" target="_blank" rel="noopener noreferrer">
-      sheets.new</a> and paste the copied table into a Google Spreadsheet`;
-
-      // Wrap the table row in a table element to preserve columns when pasting to a spreadsheet.
-      const tableHtml = `<table>${document.getElementById(scholarshipRowId)?.outerHTML?? ""}</table>`;
-      Utils.copyToClipboard(tableHtml, copyToClipBoardRowId, toastBody);
-
-    }
-
-    return (
-        <tr id={scholarshipRowId}>
-            <td>
-              <a className="btn btn-link text-align-left" href={scholarship.scholarship_url} target="_blank" rel="noopener noreferrer">
-                {scholarship.name}
-              </a>
-            </td>
-            <td>{scholarship.description}</td>
-            <td>{scholarship.notes}</td>
-            <td>{scholarship.deadline}</td>
-            <td className="text-center">
-              <a className="btn btn-link" href={`${ScholarshipUtils.generateCalendarLink(scholarship)}`} target="_blank" rel="noopener noreferrer">
-                Save to Calendar
-              </a>
-              <hr/>
-              <a className="btn btn-link remove-in-clipboard" id={copyToClipBoardRowId} onClick={copyToClipBoard}>
-                Copy
-              </a>
-              <hr/>
-              <a className="btn btn-link text-danger remove-in-clipboard">
-                Remove
-              </a>
-            </td>
-        </tr>
-    )
 }
 
 export default ScholarshipsTable;
