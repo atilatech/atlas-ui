edc952f2a037316a02070d2120dc1eb5c2d56e5c
aarondoerfler <aaron.doerfler17@gmail.com>
2021-09-10 14:45:04 -0400

    added link to atila reddit and chrome web store link

--- atila-scholarship-helper-extension-react/src/scenes/Dashboard/DashBoard.tsx
+++ atila-scholarship-helper-extension-react/src/scenes/Dashboard/DashBoard.tsx
@@ -1,8 +1,10 @@
 import ScholarshipsTable from "./ScholarshipsTable";
+import { Utils } from '../../services/Utils';
 
 function DashBoard() {
 
     return (
+        
         <div className="mt-3">
             <h1 className="text-center">
                 Atila Scholarship Helper
@@ -10,7 +12,7 @@ function DashBoard() {
             <div className="DashBoard container card shadow my-5 p-5">
                 <ScholarshipsTable />
                 <p className="text-center text-muted">
-                    Built by <a href="https://atila.ca" target="_blank" rel="noopener noreferrer">Atila</a>
+                Built by <a href="https://atila.ca" target="_blank" rel="noopener noreferrer">Atila</a> | Questions? Ask on <a href="https://www.reddit.com/r/atila/" target="_blank" rel="noopener noreferrer">r/atila</a> | Tell a Friend | View on <a href="https://chrome.google.com/webstore" target="_blank" rel="noopener noreferrer">Chrome Web Store</a>
                 </p>
             </div>
         </div>
