5c11294ccc5eebf3be5d55c48cc2276296565be5
aarondoerfler <aaron.doerfler17@gmail.com>
2021-09-11 13:55:21 -0400

    started the copy to clipboard function in the footer

--- atila-scholarship-helper-extension-react/src/scenes/Dashboard/DashBoard.tsx
+++ atila-scholarship-helper-extension-react/src/scenes/Dashboard/DashBoard.tsx
@@ -3,6 +3,16 @@ import { Utils } from '../../services/Utils';
 
 function DashBoard() {
 
+    const copyToClipBoard = () => {
+
+        const toastBody = `The link to the Atila Scholarship Helper Chrome Extension has been copied to your clipboard! `;
+
+        Utils.copyToClipboard(document.getElementById(copyToClipBoardRowId)?.outerHTML?? "", "copyChromeWebstoreLinkToClipBoard", toastBody);
+    
+     }; 
+
+     const copyToClipBoardRowId = `copyToClipBoard-${"https://chrome.google.com/webstore"}`;
+
     return (
         
         <div className="mt-3">
@@ -12,7 +22,7 @@ function DashBoard() {
             <div className="DashBoard container card shadow my-5 p-5">
                 <ScholarshipsTable />
                 <p className="text-center text-muted">
-                Built by <a href="https://atila.ca" target="_blank" rel="noopener noreferrer">Atila</a> | Questions? Ask on <a href="https://www.reddit.com/r/atila/" target="_blank" rel="noopener noreferrer">r/atila</a> | Tell a Friend | View on <a href="https://chrome.google.com/webstore" target="_blank" rel="noopener noreferrer">Chrome Web Store</a>
+                Built by <a href="https://atila.ca" target="_blank" rel="noopener noreferrer">Atila</a> | Questions? Ask on <a href="https://www.reddit.com/r/atila/" target="_blank" rel="noopener noreferrer">r/atila</a> |<a className="btn btn-link remove-in-clipboard" id={"copyChromeWebstoreLinkToClipBoard"} onClick={copyToClipBoard}>Tell a Friend</a> | View on <a href="https://chrome.google.com/webstore" target="_blank" rel="noopener noreferrer">Chrome Web Store</a>
                 </p>
             </div>
         </div>
