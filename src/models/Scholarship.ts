2f0c39e89ad90e76b11fb3d7b68d07614501c0e1
ademidun <tomiademidun@gmail.com>
2021-09-23 15:47:50 -0400

    save a scholarship to the atila database when a scholarship is saved as an extension

--- src/models/Scholarship.ts
+++ src/models/Scholarship.ts
@@ -1,8 +1,11 @@
 import { Utils } from "../services/Utils";
 
+export const SCHOLARSHIP_CREATION_SOURCE_CHROME_EXTENSION = "chrome_extension"
 export class Scholarship {
     
     public id: string = Utils.getRandomString();
+    public owner_guest_user_id: string = Utils.getRandomString();
+    public creation_source: string = SCHOLARSHIP_CREATION_SOURCE_CHROME_EXTENSION;
     public name: string = "";
     public description?: string = "";
     public scholarship_url?: string = "";
