bb3b8c88385a2cb0d819cfe397f9cf821784e897
ademidun <tomiademidun@gmail.com>
2021-09-26 09:36:38 -0400

    use new endpoint for saving scholarship from chrome extension

--- src/services/AtilaAPI.ts
+++ src/services/AtilaAPI.ts
@@ -2,7 +2,7 @@ import { Scholarship } from "../models/Scholarship";
 import Environment from "./Environment";
 
 class AtilaAPI {
-    static scholarshipsApiUrl = `${Environment.apiUrl}/scholarships/`;
+    static scholarshipsApiUrl = `${Environment.apiUrl}/scholarship`;
 
     static saveScholarship = (scholarship: Scholarship) => {
             const postData: Scholarship | any = {
@@ -16,11 +16,12 @@ class AtilaAPI {
             delete postData.date_modified;
     
             // TODO add a test to ensure that that post scholarship is called with the correct format that the Atila API expects
-            const apiResponsePromise = fetch(AtilaAPI.scholarshipsApiUrl, {
+            const apiResponsePromise = fetch(`${AtilaAPI.scholarshipsApiUrl}/create-scholarship-no-csrf-check/`, {
                 method: 'POST',
                 headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Referrer': document.location.origin
                 },
                 body: JSON.stringify({scholarship: postData})
             })
