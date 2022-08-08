7a80e0c2dc3efb91e93c9fff7f44a0ed68cefa7c
hadialhakeem <hadihakeem2@gmail.com>
2022-08-07 20:20:53 -0400

    got logging in done

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import AtilaAPI from "../../services/AtilaAPI";
 
 function Settings() {
 
-  return (
-    <div className="Settings">
-        <input className="form-control mb-3" placeholder='Username' />
-        <input className="form-control mb-3" placeholder='Password' type='password' />
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState("");
 
-    </div>
-  )
+    const login = () => {
+        setLoading("Logging in...")
+        AtilaAPI
+            .login(username, password)
+            .then(res => {
+                localStorage.setItem("jwt_token", res.token)
+            })
+    }
+
+    return (
+        <div className="Settings">
+            <input className="form-control mb-3"
+                placeholder='Username'
+                value={username}
+                onChange={event => setUsername(event.target.value)} />
+            <input className="form-control mb-3"
+                placeholder='Password'
+                type='password'
+                value={password}
+                onChange={event => setPassword(event.target.value)} />
+            <button onClick={()=>login()} className="btn btn-primary" disabled={!!loading}>
+                Login
+            </button>
+        </div>
+    )
 }
 
 export default Settings
--- src/services/Environment.ts
+++ src/services/Environment.ts
@@ -1,5 +1,5 @@
 export const EnvironmentDev = {
-    apiUrl: 'http://127.0.0.1:8080/api',
+    apiUrl: 'http://127.0.0.1:8000/api',
 };
 
 export const EnvironmentProd = {
