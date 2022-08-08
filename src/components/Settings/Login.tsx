9ab55200225c5028d1a1763871b539ba4c9b2f68
hadialhakeem <hadihakeem2@gmail.com>
2022-08-07 20:27:36 -0400

    moved functionality to another file, added ability to logout

---
+++ src/components/Settings/Login.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import AtilaAPI from "../../services/AtilaAPI";
+
+function Login() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState("");
+
+    const login = () => {
+        setLoading("Logging in...")
+        AtilaAPI
+            .login(username, password)
+            .then(res => {
+                localStorage.setItem("jwt_token", res.token)
+            })
+    }
+
+    return (
+        <div>
+            <input className="form-control mb-3"
+                   placeholder='Username'
+                   value={username}
+                   onChange={event => setUsername(event.target.value)} />
+            <input className="form-control mb-3"
+                   placeholder='Password'
+                   type='password'
+                   value={password}
+                   onChange={event => setPassword(event.target.value)} />
+            <button onClick={()=>login()} className="btn btn-primary" disabled={!!loading}>
+                Login
+            </button>
+        </div>
+    )
+}
+
+export default Login
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,34 +1,26 @@
-import React, {useState} from 'react';
-import AtilaAPI from "../../services/AtilaAPI";
+import React from 'react';
+import Login from "./Login";
 
 function Settings() {
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState("");
+    const isLoggedIn = localStorage.getItem("jwt_token")
 
-    const login = () => {
-        setLoading("Logging in...")
-        AtilaAPI
-            .login(username, password)
-            .then(res => {
-                localStorage.setItem("jwt_token", res.token)
-            })
+    if (!isLoggedIn) {
+        return (
+            <div className="Settings">
+                <Login />
+            </div>
+        )
+    }
+
+    const logOut = () => {
+        localStorage.removeItem("jwt_token")
     }
 
     return (
         <div className="Settings">
-            <input className="form-control mb-3"
-                placeholder='Username'
-                value={username}
-                onChange={event => setUsername(event.target.value)} />
-            <input className="form-control mb-3"
-                placeholder='Password'
-                type='password'
-                value={password}
-                onChange={event => setPassword(event.target.value)} />
-            <button onClick={()=>login()} className="btn btn-primary" disabled={!!loading}>
-                Login
+            <button onClick={()=>logOut()} className="btn btn-primary">
+                Log out
             </button>
         </div>
     )
