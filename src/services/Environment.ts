0656928d493e9a1d2983e008c349b4f76bf10b03
ademidun <tomiademidun@gmail.com>
2022-12-15 17:39:26 -0500

    add support for staging environments
    and update to version 1.1.4

--- src/services/Environment.ts
+++ src/services/Environment.ts
@@ -4,10 +4,17 @@ export const EnvironmentDev = {
     atilaApiUrl: 'http://127.0.0.1:8000/api'
 };
 
+
+export const EnvironmentStaging = {
+    frontendUrl: 'https://staging.atila.ca',
+    apiUrl: 'https://5dkl8h3luf.execute-api.us-east-1.amazonaws.com/staging/api',
+    atilaApiUrl: 'https://atila-7-staging.herokuapp.com/api'
+};
+
 export const EnvironmentProd = {
     frontendUrl: 'https://atila.ca',
     apiUrl: 'https://5dkl8h3luf.execute-api.us-east-1.amazonaws.com/prod/api',
-    atilaApiUrl: 'https://atila-7-staging.herokuapp.com/api'
+    atilaApiUrl: 'https://atila-7.herokuapp.com/api'
 };
 
 // set to EnvironmentDev as the default so we can use type hinting and the autocomplete feature
@@ -15,6 +22,9 @@ let Environment = EnvironmentDev;
 
 if(process.env.REACT_APP_ATILA_STAGE === "prod"){
     Environment =  EnvironmentProd;
+}
+if(process.env.REACT_APP_ATILA_STAGE === "staging"){
+    Environment =  EnvironmentStaging;
 }
 
 export default Environment;
