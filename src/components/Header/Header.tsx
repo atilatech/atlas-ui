82b1d42b16964da8c11d25539fde57e4fd60598a
Tomiwa Ademidun <tomiademidun@gmail.com>
2024-11-15 09:28:47 -0500

    check token expiry

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
-    const isLoggedIn = !!localStorage.getItem('token');
+
+  function parseJwt (token: any) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+  let isLoggedIn;
+  let jwtToken = localStorage.getItem('token');
+
+  if (jwtToken) {
+    const decoded = parseJwt(jwtToken);
+    const currentDate = Date.now(); // Get current time in milliseconds
     
+    console.log({ decoded });
+
+    // Check if the token is expired
+    if ((decoded.exp * 1000) <= currentDate) {
+      console.log('JWT expired');
+      isLoggedIn = false;
+      // localStorage.removeItem('token'); // Uncomment if you want to remove the token
+    } else {
+      isLoggedIn = true;
+    }
+  } else {
+    isLoggedIn = false;
+  }
+
+
   return (
     <header className="mt-3 py-1">
       <div className="container">
